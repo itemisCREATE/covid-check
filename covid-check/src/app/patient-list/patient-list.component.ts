8b9d73f8eeface3ddac137280e7e4cb5b41eb317
Boris <holzer@itemis.de>
2020-03-21 10:22:13 +0100

    added pagination and sorting to Patienliste

--- covid-check/src/app/patient-list/patient-list.component.ts
+++ covid-check/src/app/patient-list/patient-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {Patient} from '../model/model';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-patient-list',
@@ -8,20 +12,36 @@ import {Patient} from '../model/model';
 })
 export class PatientListComponent implements OnInit {
 
-  PATIENT_MOCK: Patient [] = [
-    { fileNumber: "123", firstname: "Manfred", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen" },
-    { fileNumber: "123", firstname: "Manfred2", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
-    { fileNumber: "123", firstname: "Manfred3", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
-    { fileNumber: "123", firstname: "Manfred4", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
-    { fileNumber: "123", firstname: "Manfred5", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
-    { fileNumber: "123", firstname: "Manfred6", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
-  ];
-  
-  dataSource = this.PATIENT_MOCK;
+ 
+  dataSource = new MatTableDataSource(PATIENT_MOCK);
   displayedColumns: string[] = ['fileNumber','firstname','lastname','dateOfBirth'];
+  @ViewChild(MatSort, {static: true}) sort: MatSort; 
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+ 
+  
   constructor() { }
-
+ 
   ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
+
+const PATIENT_MOCK: Patient [] = [
+  { fileNumber: "123", firstname: "Manfred", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen" },
+  { fileNumber: "123", firstname: "Manfred2", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred3", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred4", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred5", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred6", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred7", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred8", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred9", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred10", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred11", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred12", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred13", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred14", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+  { fileNumber: "123", firstname: "Manfred15", lastname: "Mustermann", dateOfBirth: new Date("1984-03-12"), street: "Teststr. 5", zip: "45144", city: "Lünen"},
+];
