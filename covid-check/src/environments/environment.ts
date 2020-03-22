149e6a3c5d89bce752c60843f2c39b2bb0afa93e
Benjamin Schwertfeger <benjamin.schwertfeger@itemis.de>
2020-03-22 11:13:30 +0100

    Fixed import

--- covid-check/src/environments/environment.ts
+++ covid-check/src/environments/environment.ts
@@ -1,7 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import 'credentials.ts';
 import { credentials } from './credentials';
 
 export const environment = {
